c9f0e3e698656aadbf11ecdf206b96b78d7aa479
John T <anything.gravy241@oleg1914.mailer.me>
2024-02-26 18:26:33 +0000

    update table

--- dbt/models/dwh/dwh_days.sql
+++ dbt/models/dwh/dwh_days.sql
@@ -1,7 +1,7 @@
 {{
     config(materialized='table',
         partition_by={
-            "field": "created_at",
+            "field": "created_at_truncated",
             "data_type": "timestamp",
             "granularity": "day"
         },
@@ -10,6 +10,11 @@
     )
 }}
 
-SELECT *
-FROM {{ source('dwh', 'gh_external_table') }}
-
+SELECT 
+    *,
+    -- First, convert created_at to TIMESTAMP
+    CAST(created_at AS TIMESTAMP) as created_at_timestamp,
+    -- Then use the converted timestamp in TIMESTAMP_TRUNC
+    TIMESTAMP_TRUNC(CAST(created_at AS TIMESTAMP), DAY) as created_at_truncated
+FROM 
+    `final-project-zoomcamp-2024.gh_archive_all.gh_external_table`
